0f1996a72f2028ed10d3a586af62d9f02385137a
Parker Kelley <Pklly92@gmail.com>
2020-09-22 16:22:48 -0500

    finished the project

--- scripts/TicketBooth.js
+++ scripts/TicketBooth.js
@@ -1,6 +1,6 @@
 const contentTarget = document.querySelector('.entry');
 const eventHub = document.querySelector('#state-fair');
-
+let totalTicketPurchased = 0;
 eventHub.addEventListener('click', (clickEvt) => {
 	if (clickEvt.target.id.startsWith('ride')) {
 		const rideEvent = new CustomEvent('rideTicketPurchased', {
@@ -8,6 +8,9 @@ eventHub.addEventListener('click', (clickEvt) => {
 				Athing: 'Something',
 			},
 		});
+		totalTicketPurchased++;
+		console.log(totalTicketPurchased);
+		renderTotalTicketNumber(totalTicketPurchased);
 		eventHub.dispatchEvent(rideEvent);
 	}
 });
@@ -15,6 +18,9 @@ eventHub.addEventListener('click', (clickEvt) => {
 eventHub.addEventListener('click', (clickEvent) => {
 	if (clickEvent.target.id.startsWith('food')) {
 		const foodEvent = new CustomEvent('foodTicketPurchased', {});
+		totalTicketPurchased++;
+		console.log(totalTicketPurchased);
+		renderTotalTicketNumber(totalTicketPurchased);
 		eventHub.dispatchEvent(foodEvent);
 	}
 });
@@ -22,6 +28,9 @@ eventHub.addEventListener('click', (clickEvent) => {
 eventHub.addEventListener('click', (e) => {
 	if (e.target.id.startsWith('game')) {
 		const gameEvent = new CustomEvent('gameTicketPurchased', {});
+		totalTicketPurchased++;
+		console.log(totalTicketPurchased);
+		renderTotalTicketNumber(totalTicketPurchased);
 		eventHub.dispatchEvent(gameEvent);
 	}
 });
@@ -29,6 +38,9 @@ eventHub.addEventListener('click', (e) => {
 eventHub.addEventListener('click', (clickEvent) => {
 	if (clickEvent.target.id.startsWith('side')) {
 		const sideshowEvent = new CustomEvent('sideshowTicketPurchased', {});
+		totalTicketPurchased++;
+		console.log(totalTicketPurchased);
+		renderTotalTicketNumber(totalTicketPurchased);
 		eventHub.dispatchEvent(sideshowEvent);
 	}
 });
@@ -36,13 +48,22 @@ eventHub.addEventListener('click', (clickEvent) => {
 eventHub.addEventListener('click', (clickEvent) => {
 	if (clickEvent.target.id.startsWith('full')) {
 		const fullPackageEvent = new CustomEvent('fullPackageTicketPurchased', {});
+		totalTicketPurchased++;
+		console.log(totalTicketPurchased);
+		renderTotalTicketNumber(totalTicketPurchased);
 		eventHub.dispatchEvent(fullPackageEvent);
 	}
 });
 
+const renderTotalTicketNumber = (number) => {
+	const totalTicketTarget = document.querySelector('#totalTicketParagraph');
+	return (totalTicketTarget.innerHTML = `<p>Total Tickets Purchased: ${number}</p>`);
+};
+
 export const TicketBooth = () => {
 	contentTarget.innerHTML = `
-        <div class="ticketBooth">
+				<div class="ticketBooth">
+						<div id="totalTicketParagraph"></div>
 						<button id="rideTicket">Ride Ticket</button>
 						<button id="foodTicket">Food Ticket</button>
 						<button id="gameTicket">Game Ticket</button>
